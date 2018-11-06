d88e24b1e89d5fd5eb8dfc08ee5b47127cbfdfec
Anahit <anahitvardevanyan89@gmail.com>
2018-11-06 14:35:13 +0400

    adding components UserList, UserAlbums and Photos

--- usersPhotoAlbums/src/App.js
+++ usersPhotoAlbums/src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UserList from './UserList/UserList.js';
+import UserAlbums from './UserAlbums/UserAlbums.js';
+import Photos from './Photos/Photos.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
-        <div className="main">
-          photo album
+      <Router>
+        <div className='main'>
+          <Route 
+            path='/' 
+            component={UserList} 
+          />
+          <Route 
+            path='/users/:id' 
+            component={UserAlbums}
+          />
+          <Route 
+            path='/users/:id/albums/:id' 
+            component={Photos} 
+          />
         </div>
+      </Router>
     );
   }
 }
 
 export default App;
-
-
-
-
-
-
